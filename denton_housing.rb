8e374af7eda056812d0fbaf416591bddf5a47eef
Dave Sims <davsims@gmail.com>
2017-02-05 21:48:08 -0600

    Add a couple more enpoints

--- denton_housing.rb
+++ denton_housing.rb
@@ -5,19 +5,37 @@ require_relative './denton_data'
 
 class DentonHousing
 
-  def self.get_housing
+  def self.connection
     conn = DentonData.connection
     conn = PG.connect(
       :dbname => 'denton_data',
       :user => 'denton_data',
       :password => 'password1'
     )
+  end
+
+  def self.get_housing
+    conn = DentonHousing.connection
     records = conn.exec("SELECT * FROM denton_housing")
     records.map do |record|
       record.to_hash
     end
   end
 
-end
+  def self.total_housing_units(year)
+    if year
+      DentonHousing.connection.exec(
+        "select total_housing_units from denton_housing where year = #{year};"
+      )
+    end
+  end
 
-DentonHousing.get_housing
+  # total_housing_units | vacant_housing_units | occupied_housing_units | owner_occupied | median_gross_rent | renter_occupied
+  def self.vacant_housing_units(year)
+    if year
+      DentonHousing.connection.exec(
+        "select vacant_housing_units from denton_housing where year = #{year};"
+      )
+    end
+  end
+end
--- server.rb
+++ server.rb
@@ -4,3 +4,11 @@ require_relative './denton_housing'
 get '/denton-housing' do
   DentonHousing.get_housing.to_json
 end
+
+get '/total-housing-units' do
+  DentonHousing.total_housing_units(params["year"])
+end
+
+get '/vacant_housing_unit' do
+  DentonHousing.vacant_housing_unit(params["year"])
+end
