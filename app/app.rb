698e722280908646485a077eb4081f6d02eda62b
Dave Sims <davsims@gmail.com>
2017-02-06 08:29:22 -0600

    A little reorg

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require_relative './denton_housing'
+require_relative './models/denton_housing'
 
 get '/denton-housing' do
   DentonHousing.get_housing.to_json
--- schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE denton_housing (
-  year integer,
-  total_housing_units integer,
-  vacant_housing_units integer,
-  occupied_housing_units integer,
-  owner_occupied integer,
-  median_gross_rent integer,
-  renter_occupied integer
-);
