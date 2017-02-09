d976f0572a5e91712b21487bdaabbf441a0ee3f8
Alexa <alexa.anderson@outlook.com>
2017-02-09 09:06:02 -0600

    Uncomment original

--- app/app.rb
+++ app/app.rb
@@ -1,21 +1,21 @@
 require 'sinatra'
-# require_relative './models/denton_housing'
+require_relative './models/denton_housing'
 require "sinatra/activerecord"
 require_relative './models/well_inspection'
 
 set :database_file, "../config/database.yml"
 
-# get '/denton-housing' do
-#   DentonHousing.get_housing.to_json
-# end
+get '/denton-housing' do
+  DentonHousing.get_housing.to_json
+end
 
-# get '/total-housing-units' do
-#   DentonHousing.total_housing_units(params["year"])
-# end
+get '/total-housing-units' do
+  DentonHousing.total_housing_units(params["year"])
+end
 
-# get '/vacant_housing_units' do
-#   DentonHousing.vacant_housing_units(params["year"])
-# end
+get '/vacant_housing_units' do
+  DentonHousing.vacant_housing_units(params["year"])
+end
 
 get '/' do
   time = Time.now
